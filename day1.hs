3f7a5c1abcead4e5627853828e850da96e3d406d
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-12-01 09:31:43 +0000

    wip day1 - presumably there's std library calls that would make this easier

---
+++ day1.hs
@@ -0,0 +1,39 @@
+main :: IO ()
+main = do
+  input <- readFile "example1.txt"
+  let output = solve input
+   in putStrLn ("Answer: " ++ (show output))
+
+solve :: String -> Integer
+solve input = 7
+
+rev :: [a] -> [a]
+rev lst = aux [] lst
+  where
+    aux :: [a] -> [a] -> [a]
+    aux acc l =
+      case l of
+        [] -> acc
+        h:t -> aux (h : acc) t
+
+getNextLine :: String -> (String, String)
+getNextLine str = aux [] str
+  where
+    aux :: String -> String -> (String, String)
+    aux acc s =
+      case s of
+        [] -> (rev acc, [])
+        h:t ->
+          if h == '\n'
+            then (rev acc, t)
+            else aux (h : acc) t
+
+toLines :: String -> [String]
+toLines str = aux [] str
+  where
+    aux :: [String] -> String -> [String]
+    aux acc s =
+      let (next, rest) = getNextLine s
+       in if rest == []
+            then rev (next : acc)
+            else aux (next : acc) rest
