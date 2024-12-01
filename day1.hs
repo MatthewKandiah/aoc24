45e9d81b7feaa82d24d176a11f1c3ab8d2e08898
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-12-01 10:23:36 +0000

    day 1 part 1 done

--- day1.hs
+++ day1.hs
@@ -1,11 +1,22 @@
+import Data.Char -- for isSpace
+import Data.List -- for dropWhileEnd
+
 main :: IO ()
 main = do
-  input <- readFile "example1.txt"
-  let output = toLines input
+  input <- readFile "input1.txt"
+  let output = solve input
    in putStrLn $ show output
 
 solve :: String -> Integer
-solve input = 7
+solve input =
+  sum
+    $ absDiffColumns
+    $ sortColumns
+    $ toColumns
+    $ map toIntegers
+    $ map toSplitLine
+    $ filter isNotEmpty
+    $ toLines input
 
 rev :: [a] -> [a]
 rev lst = aux [] lst
@@ -35,6 +46,42 @@ toLines str = aux [] str
     aux acc s =
       let (next, rest) = getNextLine s
        in if rest == []
-            then rev (next : acc)
+            then rev (map strip (next : acc))
             else aux (next : acc) rest
 
+isNotEmpty :: String -> Bool
+isNotEmpty str = str /= []
+
+toSplitLine :: String -> (String, String)
+toSplitLine str = aux [] str
+  where
+    aux :: String -> String -> (String, String)
+    aux acc s =
+      case s of
+        [] -> (strip (rev acc), [])
+        h:t ->
+          if (isSpace h)
+            then (strip (rev acc), strip t)
+            else aux (h : acc) t
+
+strip :: String -> String
+strip = dropWhile isSpace . dropWhileEnd isSpace
+
+toIntegers :: (String, String) -> (Integer, Integer)
+toIntegers (left, right) = (read left, read right)
+
+toColumns :: [(a, a)] -> ([a], [a])
+toColumns input = aux [] [] input
+  where
+    aux leftAcc rightAcc lrList =
+      case lrList of
+        [] -> (rev leftAcc, rev rightAcc)
+        (lh, rh):t -> aux (lh : leftAcc) (rh : rightAcc) t
+
+sortColumns :: Ord a => ([a], [a]) -> ([a], [a])
+sortColumns (left, right) = (sort left, sort right)
+
+absDiffColumns :: Num a => ([a], [a]) -> [a]
+absDiffColumns (l, r) = zipWith absoluteDifference l r
+  where
+    absoluteDifference x y = abs $ x - y
