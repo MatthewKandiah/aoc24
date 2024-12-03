e53fb459dc89f795df43d5f8c9930f9594aba7d0
Matthew Kandiah <mj.kandiah@googlemail.com>
2024-12-03 04:22:18 +0000

    collapse utils back into day1 - turns out none of it was really that useful

--- day1/solution.hs
+++ day1/solution.hs
@@ -1,6 +1,5 @@
 import Data.Char -- for isSpace
 import Data.List -- for dropWhileEnd
-import Utils
 
 main :: IO ()
 main = do
@@ -81,3 +80,29 @@ absDiffColumns :: Num a => ([a], [a]) -> [a]
 absDiffColumns (l, r) = zipWith absoluteDifference l r
   where
     absoluteDifference x y = abs $ x - y
+
+getNextLine :: String -> (String, String)
+getNextLine = aux []
+  where
+    aux :: String -> String -> (String, String)
+    aux acc s =
+      case s of
+        [] -> (reverse acc, [])
+        h:t ->
+          if h == '\n'
+            then (reverse acc, t)
+            else aux (h : acc) t
+
+toLines :: String -> [String]
+toLines = aux []
+  where
+    aux :: [String] -> String -> [String]
+    aux acc s =
+      let (next, rest) = getNextLine s
+       in if null rest
+            then reverse (map strip (next : acc))
+            else aux (next : acc) rest
+
+strip :: String -> String
+strip = dropWhile isSpace . dropWhileEnd isSpace
+
